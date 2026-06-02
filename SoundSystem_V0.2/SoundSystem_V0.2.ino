d32cff6092ce04f40a49cf933139f86896aca54e
Zakary CHAUVE <141487930+MountainEnjoyer@users.noreply.github.com>
2026-06-02 14:12:52 +0200

    Weird Sequencer

--- SoundSystem_V0.2/SoundSystem_V0.2.ino
+++ SoundSystem_V0.2/SoundSystem_V0.2.ino
@@ -1,113 +1,47 @@
-// include the library code:
-#include <LiquidCrystal.h>
-
-// download the library ButtonFever for the RotaryEncoder
-#include <BfButton.h>
-
 // Pin's Numbers
-int Speaker1 = 13;
-int HertzModule = 1;
-int Plus = 2;
-int Moins = 4;
-int btnPin = 50;
-int DT = 51;
-int CLK = 52;
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-BfButton btn(BfButton::STANDALONE_DIGITAL, btnPin, true, LOW);
+#define Speaker 11
 
 // Variables
-int value = 0;
-int Ecart = 0;
+int speed;
+int freq1;
+int freq2;
+int freq3;
+int freq4;
+int freq5;
+int freq6;
+int freq7;
 
-int counter = 0;
-int angle = 0;
-int aState;
-int aLastState;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(Speaker1, OUTPUT);
-  pinMode(CLK, INPUT_PULLUP);
-  pinMode(DT, INPUT_PULLUP);
-  pinMode(Moins, INPUT);
-  pinMode(Plus, INPUT);
-  aLastState = digitalRead(CLK);
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  //button settings
-  btn.onPress(pressHandler)
-  .onDoublePress(pressHandler) // default timeout
-  .onPressFor(pressHandler, 1000); // cutstom timeout for 1 second
 }
 
-void pressHandler (BfButton *btn, BfButton::press_pattern_t pattern) {
-  switch (pattern) {
-    case BfButton::SINGLE_PRESS:
-      Serial.println("Single push");
-      break;
-      
-    case BfButton::DOUBLE_PRESS:
-      Serial.println("Double push");
-      break;
-      
-    case BfButton::LONG_PRESS:
-      Serial.println("Long push");
-      break;
-  }
-}
 
 void loop() {
-  btn.read();
+  //freq
+  freq1 = map(analogRead(A1), 0, 1023, 30, 800);
+  freq2 = map(analogRead(A2), 0, 1023, 30, 800);  
+  freq3 = map(analogRead(A3), 0, 1023, 30, 800);
+  freq4 = map(analogRead(A4), 0, 1023, 30, 800);
+  freq5 = map(analogRead(A5), 0, 1023, 30, 800);
+  freq6 = map(analogRead(A6), 0, 1023, 30, 800);
+  freq7 = map(analogRead(A7), 0, 1023, 30, 800);
+  //bpm
+  speed = map(analogRead(A0), 0, 1023, 0, 1000);
 
+  tone(Speaker, freq1);
+  delay(speed);
+  tone(Speaker, freq2);
+  delay(speed);
+  tone(Speaker, freq3);
+  delay(speed);
+  tone(Speaker, freq4);
+  delay(speed);
+  tone(Speaker, freq5);
+  delay(speed);
+  tone(Speaker, freq6);
+  delay(speed);
+  tone(Speaker, freq7);
+  delay(speed);
 
-  aState = digitalRead(CLK);
- 
-  //Encoder rotation tracking
-  if (aState != aLastState){     
-     if (digitalRead(DT) != aState) { 
-       counter ++;
-       angle ++;
-     }
-     else {
-       counter--;
-       angle --;
-     }
-     if (counter >=100 ) {
-       counter =100;
-     }
-     if (counter <=-100 ) {
-       counter =-100;
-     }
-     Serial.println(counter); 
-  }   
-  aLastState = aState;
-
-  // Serial.println("l'Ecart est de ");
-  // Serial.println(Ecart);
-  lcd.setCursor(0, 0);
-  value = analogRead(HertzModule);
-
-  if (Ecart > 0 ){
-    tone(Speaker1, value);
-    delay(Ecart);
-    noTone(Speaker1);
-    delay(Ecart);
-  } else { 
-    tone(Speaker1, value);
-  }
-  // print the number of seconds since reset:
-  lcd.print(value);
-  lcd.print(" Hertz");
-  lcd.setCursor(0, 1);
-  lcd.print("Ecart de ");
-  lcd.print(Ecart);
-  lcd.print("ms");
-  if ( digitalRead(Plus) == 1) {
-    Ecart = Ecart + 10;
-  } 
-  if ( digitalRead(Moins) == 1 && Ecart > 0) {
-    Ecart = Ecart - 10;
-  }
 }
