06a8b2807f4ad16b945114e19126562a43846dd0
Zakary CHAUVE <141487930+MountainEnjoyer@users.noreply.github.com>
2026-06-03 10:59:05 +0200

    init Pygame and variables

---
+++ SoundSystem_V0.2.1/GUI.py
@@ -0,0 +1,19 @@
+import math
+import time
+
+from math import cos, sin
+
+import serial
+import pygame 
+
+pygame.init()
+
+WIDTH = 400
+HEIGHT = 400
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+arduinoData = serial.Serial('com3', 9600)
+timer = pygame.time.Clock()
+fps = 60
+data = [0, 0, 0, 0, 0, 0, 0, 0] 
+font = pygame.font.Font('freesansbold.ttf', 24)
