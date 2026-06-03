aafa3cb6a7bd34acbcb820a910744b0974acf488
Zakary CHAUVE <141487930+MountainEnjoyer@users.noreply.github.com>
2026-06-03 10:49:15 +0200

    Starting A Python GUI

---
+++ SoundSystem_V0.2.1/SoundSystem_V0.2.1.ino
@@ -0,0 +1,64 @@
+// Pin's Numbers
+#define Speaker 11
+
+// Variables
+int speed1;
+int speed2;
+int speed3;
+int speed4;
+
+int freq1;
+int freq2;
+int freq3;
+int freq4;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  //bpm
+  speed1 = map(analogRead(A0), 0, 1023, 0, 1000);
+  speed2 = map(analogRead(A2), 0, 1023, 0, 1000);
+  speed3 = map(analogRead(A4), 0, 1023, 0, 1000);  
+  speed4 = map(analogRead(A6), 0, 1023, 0, 1000);
+
+
+  //freq
+  freq1 = map(analogRead(A1), 0, 1023, 0, 1000);
+  freq2 = map(analogRead(A3), 0, 1023, 0, 1000);
+  freq3 = map(analogRead(A5), 0, 1023, 0, 1000);
+  freq4 = map(analogRead(A7), 0, 1023, 0, 1000);
+
+
+  if(speed1 < 20 && speed2 < 20 && speed3 < 20 && speed4 < 20) {
+    noTone(Speaker);
+  }
+  if ( speed1 > 40) {
+    if ( freq1 > 40) {
+      tone(Speaker, freq1);
+    }
+  }
+  delay(speed1);
+  if ( speed2 > 40) {
+    if ( freq2 > 40) {
+      tone(Speaker, freq2);
+    }
+  }
+  delay(speed2);
+  if ( speed3 > 40) {
+    if ( freq3 > 40) {
+      tone(Speaker, freq3);
+    }
+  }
+  delay(speed3);
+  if ( speed4 > 40) {
+    if ( freq4 > 40) {
+      tone(Speaker, freq4);
+    }
+  }
+  delay(speed4);
+
+}
