14b2d22f2b3d048f4073e675271de828518f47af
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-04 18:52:48 +0200

    Linux script + better arduino

--- SoundSystem_V0.2.1/SoundSystem_V0.2.1.ino
+++ SoundSystem_V0.2.1/SoundSystem_V0.2.1.ino
@@ -25,7 +25,7 @@ void loop() {
   //bpm
   speed1 = map(analogRead(A0), 0, 1023, 0, 999);
   speed2 = map(analogRead(A2), 0, 1023, 0, 999);
-  speed3 = map(analogRead(A4), 0, 1023, 0, 999);  
+  speed3 = map(analogRead(A4), 0, 1023, 0, 999);
   speed4 = map(analogRead(A6), 0, 1023, 0, 999);
 
 
@@ -36,35 +36,34 @@ void loop() {
   freq4 = map(analogRead(A7), 0, 1023, 0, 999);
 
 
-  if(speed1 < 20 && speed2 < 20 && speed3 < 20 && speed4 < 20) {
+  if (speed1 < 20 && speed2 < 20 && speed3 < 20 && speed4 < 20) {
     noTone(Speaker);
   }
-  if ( speed1 > 40) {
-    if ( freq1 > 40) {
+  if (speed1 > 40) {
+    if (freq1 > 40) {
       tone(Speaker, freq1);
     }
+    delay(speed1);
   }
-  delay(speed1);
-  if ( speed2 > 40) {
-    if ( freq2 > 40) {
+  if (speed2 > 40) {
+    if (freq2 > 40) {
       tone(Speaker, freq2);
     }
+    delay(speed2);
   }
-  delay(speed2);
-  if ( speed3 > 40) {
-    if ( freq3 > 40) {
+  if (speed3 > 40) {
+    if (freq3 > 40) {
       tone(Speaker, freq3);
     }
+    delay(speed3);
   }
-  delay(speed3);
-  if ( speed4 > 40) {
-    if ( freq4 > 40) {
+  if (speed4 > 40) {
+    if (freq4 > 40) {
       tone(Speaker, freq4);
     }
+    delay(speed4);
   }
-  delay(speed4);
 
   formatted_str = String(speed1) + '-' + String(freq1) + '/' + String(speed2) + '-' + String(freq2) + '/' + String(speed3) + '-' + String(freq3) + '/' + String(speed4) + '-' + String(freq4) + '/';
-  Serial.println(formatted_str); 
-
+  Serial.println(formatted_str);
 }
